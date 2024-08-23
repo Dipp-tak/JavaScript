e5d3ef9653a1313df343397b7a75924a7c4bdd05
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-23 15:45:41 +0530

    Create comment.js
    Comments help us understand the code.

---
+++ comment.js
@@ -0,0 +1,5 @@
+// This is a multi-line comment in this code.
+/*This is a multi-line
+        comment in this code.*/
+        
+console.log("We use // for single-line comment and /**/ for multi-line comment .  ");
