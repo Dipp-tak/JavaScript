65719db7953a26ef95c3d74247d8a05190d50d41
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-23 15:42:02 +0530

    Create TheFortuneTeller.js
    Variables are containers for storing data.

---
+++ Variables/TheFortuneTeller.js
@@ -0,0 +1,10 @@
+//Using the var key word, we create multiple variables, and the variables names are name,YourChild, etc.
+
+var name = "Dipp";
+var YourJob = "AI developer"
+var Yourchild = 'two';
+var MarAge= 27;
+
+var YourFortune = name+", You have become an"+" "+YourJob+". "+"You will marry your partner at the age of"+" "+MarAge+". "+"You have"+" "+Yourchild+" "+"children with your partner.";
+
+console.log(YourFortune);
