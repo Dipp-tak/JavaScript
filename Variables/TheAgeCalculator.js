64bc8b48d7d2b5ffbc02f78545bf6691a710cd6a
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-24 00:22:54 +0530

    Create TheAgeCalculator.js
    What a silly calculator !!!

---
+++ Variables/TheAgeCalculator.js
@@ -0,0 +1,6 @@
+//In this code, I calculate my age by subtracting my birth-year from 2024. 
+var MyDOB = 2004;
+var FutureYear = 2024;
+
+var age = FutureYear-MyDOB;
+console.log("So currently, I am "+age+" or "+(age-1)+" years old.")
