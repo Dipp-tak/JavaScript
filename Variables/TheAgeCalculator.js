65864a9d06363121404477c690a57904a9792a14
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-24 00:25:05 +0530

    Update TheAgeCalculator.js

--- Variables/TheAgeCalculator.js
+++ Variables/TheAgeCalculator.js
@@ -1,6 +1,7 @@
 //In this code, I calculate my age by subtracting my birth-year from 2024. 
+// In this code MyDOB, FutureYear and Myage is a variable.
 var MyDOB = 2004;
 var FutureYear = 2024;
 
-var age = FutureYear-MyDOB;
-console.log("So currently, I am "+age+" or "+(age-1)+" years old.")
+var Myage = FutureYear-MyDOB;
+console.log("So currently, I am "+Myage+" or "+(Myage-1)+" years old.");
