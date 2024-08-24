b821969687f05d1598048890350a9cee4dd6ae72
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-24 21:48:56 +0530

    Create Var-Case-Sensitive.js

---
+++ Variables/Var-Case-Sensitive.js
@@ -0,0 +1,10 @@
+//In this code, I am using four variables: `myfavtplace`, `Myfavtplace`, `MyFavtplace`, and `MyFavtPlace`. Due to the case-sensitive nature of the language, these are all considered distinct variables.
+
+
+var myfavtplace = "Darjeeling";
+var Myfavtplace = "Kolkata";
+var MyFavtplace = "hill station";
+var MyFavtPlace = "Kolkata's weather";
+
+console.log("For a long time, I have been staying in "+Myfavtplace+", but I starting to feel bored with "+MyFavtPlace+", so I decided to visit "+myfavtplace+" because I love the weather at the "+MyFavtplace+".");
+
