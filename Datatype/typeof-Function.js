b3bb6c42d126ba903ebb416d6d0659c2d105df27
Diptak Mahata <147260816+Dipp-tak@users.noreply.github.com>
2024-08-25 10:20:42 +0530

    Update and rename string.js to typeof-Function.js

---
+++ Datatype/typeof-Function.js
@@ -0,0 +1,31 @@
+// I used the 'typeof()' function to show the datatypes of variables.
+
+//This is Integers datatype
+var num =25;
+console.log(num);
+console.log(typeof(num));
+
+//This is String datatype
+var str ="Hello World!";
+console.log(str);
+console.log(typeof(str));
+
+//This is Boolean datatype
+var bool = true;
+console.log(bool);
+console.log(typeof(bool));
+
+//This is Array datatype
+var ary = ["HTML","CSS","JS"];
+console.log(ary);
+console.log(typeof(ary));
+
+//This is Object datatype
+var obj = {Name:"Diptak",Love:"JS"};
+console.log(obj);
+console.log(typeof(obj));
+
+//This is undefined datatype
+var Null;
+console.log(Null);
+console.log(typeof(Null));
